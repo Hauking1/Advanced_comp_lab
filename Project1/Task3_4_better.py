73c7507d51a97fad7a8333c348c890cf4413011b
Hauke <h.uhde@tu-braunschweig.de>
2025-06-08 18:00:08 +0200

    updated the whole project now with 3_4 done and 3_5 only calculations needed :)

--- Project1/Task3_4_better.py
+++ Project1/Task3_4_better.py
@@ -80,14 +80,6 @@ def pd_spectral_densitiy(cavities:np.ndarray):
 def pd_typical_cavity_variance(cavities:np.ndarray)->float:
     return np.exp(np.average(np.log(np.imag(np.divide(1j,cavities)))))
 
-@nb.njit()
-def fast_3_4(precision:float,max_itter:int,populationsize:int,c:int,disorder:float,lambd:float,epsilon:float):
-    return pd_typical_cavity_variance(pd_equalibrium_single_lambda(precision,max_itter,populationsize,c,disorder,lambd,epsilon))
-
-@nb.njit()
-def const_fast_3_4(num_itter:int,populationsize:int,c:int,disorder:float,lambd:float,epsilon:float):
-    return pd_typical_cavity_variance(const_pd_equalibrium_single_lambda(num_itter,populationsize,c,disorder,lambd,epsilon))
-
 @nb.njit(parallel=True,nogil=True)
 def parrallel_const_fast_3_4(num_itter:int,populationsize:int,c:int,disorders:np.ndarray,lambd:float,epsilon:float):
     res = np.zeros(len(disorders))
@@ -96,61 +88,21 @@ def parrallel_const_fast_3_4(num_itter:int,populationsize:int,c:int,disorders:np
         res[index] = pd_typical_cavity_variance(const_pd_equalibrium_single_lambda(num_itter,populationsize,c,disorders[index],lambd,epsilon))
     return res
 
-@nb.njit()
-def fast_3_5(n_measurements:int,precision:float,max_itter:int,populationsize:int,c:int,disorder:float,lambd:float,epsilon:float):
-    return pd_typical_cavity_variance(pd_measurement(n_measurements,pd_equalibrium_single_lambda(precision,max_itter,populationsize,c,disorder,lambd,epsilon),c,lambd,epsilon,disorder))
+@nb.njit(parallel=True,nogil=True)
+def parrallel_const_fast_3_5(n_measurements:int,num_itter:int,populationsize:int,c:int,disorders:np.ndarray,lambd:float,epsilon:float):
+    res = np.zeros(len(disorders))
+    length_disorder = len(disorders)
+    for index in nb.prange(length_disorder):
+        res[index] = pd_typical_cavity_variance(pd_measurement(n_measurements,const_pd_equalibrium_single_lambda(num_itter,populationsize,c,disorders[index],lambd,epsilon),c,lambd,epsilon,disorders[index]))
+    return res
 
-def Averaged_Task_3_4(save_plots_to:str):
-    """Task 3.4 with a variang number of itterations such that the time for each cycle adds up to twenty seconds.
-    then averaded over the trials"""
-
-    precision = 5*1e-4
-    max_itter = 100000
-    populationsizes = [10**3,5*10**3,10**4]
-    c = 3
-    disorders = np.arange(10,21)
-    lambd = 0.
-    epsilon = 1e-300
-
-    start_pd = time.time()
-    typ_gs = np.zeros((len(populationsizes),len(disorders)))
-    for index_pop,populationsize in enumerate(populationsizes):
-        for index_dis,disorder in enumerate(disorders):
-            start_tot = time.time()
-            ges_time = 0
-            results_for_average = []
-            num_itters = 0
-            while ges_time<20:
-                start = time.time()
-                results_for_average.append(fast_3_4(precision,max_itter,populationsize,c,disorder,lambd,epsilon))
-                ges_time+=time.time()-start
-                num_itters+=1
-            typ_gs[index_pop][index_dis] = np.average(results_for_average)
-            print(f"pop: {populationsize}, dis {disorder}, time: {time.time()-start_tot:.2f}, itters: {num_itters}")
-        print(f"populationsize: {populationsize} done :)")
-    end_pd = time.time()-start_pd
-    
-    print("\n")
-    print(f"time for populationdynamics: {end_pd:.2f}")
-
-    for index in range(len(populationsizes)):
-        plt.plot(disorders,typ_gs[index],label = f"pop.size: {populationsizes[index]}")
-    plt.grid()
-    plt.legend()
-    plt.xlabel("Disorder: W")
-    plt.ylabel(r"$g^{typ}$")
-    plt.yscale("log")
-    plt.title(r"$g^{typ}$ for $\epsilon=$"+f"{epsilon:.0e} and for different populationsizes")
-    plt.savefig(save_plots_to+"Task_3_4_averaged")
-    plt.show()
-    plt.close()
 
 def Task_3_4_parallel(save_plots_to:str,save_data_to:str):
     """Task 3.4 with a constant number of sweeps as sugested."""
-    itters = 10000
+    itters = 50000
     populationsizes = [10**3,5*10**3,10**4]
     c = 3
-    disorders = np.arange(10,21,0.5)
+    disorders = np.arange(10,21,0.1)
     lambd = 0.
     epsilon = 1e-300
 
@@ -189,92 +141,47 @@ def Task_3_4_parallel(save_plots_to:str,save_data_to:str):
             lines.append(string_line[:-2]+"\n")
         file.writelines(lines)
 
-def Task_3_4_zoomed_in(save_plots_to:str,save_data_to:str):
-    """Task 3.4 with a constant number of sweeps as sugested."""
-    itters = 7600
-    populationsizes = [10**3,5*10**3,10**4]
-    c = 3
-    disorders = np.arange(14,19.5,0.1)
-    lambd = 0.
-    epsilon = 1e-300
 
+def Task_3_5_parallel(save_plots_to:str,save_data_to:str):
+    """Task 3.4 with a constant number of sweeps as sugested."""
+    itters = 50000
+    populationsize = 10**4
+    c = 3
+    disorders = np.arange(10,21,0.1)
+    lambd = 0.
+    epsilon = 1e-6
+    n_mes = 10**3
+    time_now = time.gmtime()
+    print(f"starting at: {time_now.tm_hour+2}:{time_now.tm_min} o'Clock")
     start_pd = time.time()
-    typ_gs = np.zeros((len(populationsizes),len(disorders)))
-    for index_pop,populationsize in enumerate(populationsizes):
-        start_pop = time.time()
-        for index_dis,disorder in enumerate(disorders):
-            start_tot = time.time()
-            typ_gs[index_pop][index_dis]=const_fast_3_4(itters,populationsize,c,disorder,lambd,epsilon)
-            print(f"pop: {populationsize}, dis {disorder}, itters:{itters}, time: {time.time()-start_tot:.2f}")
-        print(f"populationsize: {populationsize} done :) after: {time.time()-start_pop:.2f}")
+    typ_Gs=parrallel_const_fast_3_5(n_mes,itters,populationsize,c,disorders,lambd,epsilon)
     end_pd = time.time()-start_pd
+
+    time_now = time.gmtime()
+    print(f"ending at: {time_now.tm_hour+2}:{time_now.tm_min} o'Clock")
     
     print("\n")
     print(f"time for populationdynamics: {end_pd:.2f}")
 
-    for index in range(len(populationsizes)):
-        plt.plot(disorders,typ_gs[index],label = f"pop.size: {populationsizes[index]}")
+    plt.plot(disorders,typ_Gs,label = f"pop.size: {populationsize}")
     plt.grid()
     plt.legend()
     plt.xlabel("Disorder: W")
-    plt.ylabel(r"$g^{typ}$")
+    plt.ylabel(r"$G^{typ}$")
     plt.yscale("log")
-    plt.title(r"$g^{typ}$ for $\epsilon=$"+f"{epsilon:.0e} and for different populationsizes")
-    plt.savefig(save_plots_to+f"Task_3_4_zoom_{itters}")
+    plt.title(r"$G^{typ}$ for $\epsilon=$"+f"{epsilon:.0e} and for"+r"$N_{pop}=$"+f"{populationsize} with {itters} sweeps")
+    plt.savefig(save_plots_to+f"Task_3_5_{itters}")
     plt.show()
     plt.close()
 
-    with open(save_data_to+f"Task_3_4_zoom_{itters}","w") as file:
-        file.writelines(["Disorder\t"]+[str(pop)+"\t" for pop in populationsizes]+["\n"])
-        lines = []
-        for index in range(len(disorders)):
-            string_line = f"{disorders[index]}\t"
-            for pop_index in range(len(populationsizes)):
-                string_line+=str(typ_gs[pop_index][index])+"\t"
-            lines.append(string_line[:-2]+"\n")
-        file.writelines(lines)
-
-def Task_3_4_zoomed_parallel(save_plots_to:str,save_data_to:str):
-    """Task 3.4 with a constant number of sweeps as sugested."""
-    itters = 7500
-    populationsizes = [10**3,5*10**3,10**4]
-    c = 3
-    disorders = np.arange(14,19.5,0.1)
-    lambd = 0.
-    epsilon = 1e-300
-
-    start_pd = time.time()
-    typ_gs = np.zeros((len(populationsizes),len(disorders)))
-    for index_pop,populationsize in enumerate(populationsizes):
-        start_tot = time.time()
-        typ_gs[index_pop]=parrallel_const_fast_3_4(itters,populationsize,c,disorders,lambd,epsilon)
-        print(f"populationsize: {populationsize} done :) after:{time.time()-start_tot:.2f}")
-    end_pd = time.time()-start_pd
-    
-    print("\n")
-    print(f"time for populationdynamics: {end_pd:.2f}")
-
-    for index in range(len(populationsizes)):
-        plt.plot(disorders,typ_gs[index],label = f"pop.size: {populationsizes[index]}")
-    plt.grid()
-    plt.legend()
-    plt.xlabel("Disorder: W")
-    plt.ylabel(r"$g^{typ}$")
-    plt.yscale("log")
-    plt.title(r"$g^{typ}$ for $\epsilon=$"+f"{epsilon:.0e} and for different populationsizes")
-    plt.savefig(save_plots_to+f"Task_3_4_zoom_{itters}")
-    plt.show()
-    plt.close()
-
-    with open(save_data_to+f"Task_3_4_zoom_{itters}.txt","w") as file:
-        header = ["Disorder\t"]+[str(pop)+"\t" for pop in populationsizes]
+    with open(save_data_to+f"Task_3_5_{itters}.txt","w") as file:
+        header = ["Disorder\t"+str(populationsize)+"\n"]
         header[0] = header[-1][:-2]+"\n"
         file.writelines(header)
         lines = []
         for index in range(len(disorders)):
             string_line = f"{disorders[index]}\t"
-            for pop_index in range(len(populationsizes)):
-                string_line+=str(typ_gs[pop_index][index])+"\t"
+            string_line+=str(typ_Gs[index])+"\t"
             lines.append(string_line[:-2]+"\n")
         file.writelines(lines)
 
@@ -284,7 +191,5 @@ if __name__=="__main__":
     directory_path = "\\".join(__file__.split("\\")[:-1])+"\\"+"plots\\"
     data_directory_path = "\\".join(__file__.split("\\")[:-1])+"\\"+"data\\"
 
-    #Averaged_Task_3_4(directory_path)
-    Task_3_4_parallel(directory_path,data_directory_path)
-    #Task_3_4_zoomed_in(directory_path,data_directory_path)
-    #Task_3_4_zoomed_parallel(directory_path,data_directory_path)
+    #Task_3_4_parallel(directory_path,data_directory_path)
+    Task_3_5_parallel(directory_path,data_directory_path)
